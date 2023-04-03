919f66db5b3c320b712dedb5840ab49a17713bf6
bugg <13475570+soft-bugg@users.noreply.github.com>
2023-04-02 20:21:35 -0600

    set up gui scaffolding

---
+++ Sketchpop/Main_Window_Form.Designer.cs
@@ -0,0 +1,201 @@
+﻿using System.Drawing;
+
+namespace Sketchpop
+{
+    partial class main_window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exit_button = new System.Windows.Forms.Button();
+            this.title_bar = new System.Windows.Forms.Panel();
+            this.minimize_button = new System.Windows.Forms.Button();
+            this.fullscreen_button = new System.Windows.Forms.Button();
+            this.tool_bar = new System.Windows.Forms.Panel();
+            this.title_label = new System.Windows.Forms.Label();
+            this.left_settings_panel = new System.Windows.Forms.Panel();
+            this.quick_launch_bar = new System.Windows.Forms.Panel();
+            this.canvas_panel = new System.Windows.Forms.Panel();
+            this.right_settings_panel = new System.Windows.Forms.Panel();
+            this.canvas_frame = new System.Windows.Forms.PictureBox();
+            this.title_bar.SuspendLayout();
+            this.canvas_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas_frame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exit_button
+            // 
+            this.exit_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exit_button.Location = new System.Drawing.Point(1260, 0);
+            this.exit_button.MaximumSize = new System.Drawing.Size(20, 20);
+            this.exit_button.MinimumSize = new System.Drawing.Size(20, 20);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(20, 20);
+            this.exit_button.TabIndex = 1;
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // title_bar
+            // 
+            this.title_bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(167)))), ((int)(((byte)(167)))), ((int)(((byte)(167)))));
+            this.title_bar.Controls.Add(this.title_label);
+            this.title_bar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.title_bar.Enabled = false;
+            this.title_bar.Location = new System.Drawing.Point(0, 0);
+            this.title_bar.Name = "title_bar";
+            this.title_bar.Size = new System.Drawing.Size(1280, 20);
+            this.title_bar.TabIndex = 2;
+            // 
+            // minimize_button
+            // 
+            this.minimize_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimize_button.Location = new System.Drawing.Point(1220, 0);
+            this.minimize_button.MaximumSize = new System.Drawing.Size(20, 20);
+            this.minimize_button.MinimumSize = new System.Drawing.Size(20, 20);
+            this.minimize_button.Name = "minimize_button";
+            this.minimize_button.Size = new System.Drawing.Size(20, 20);
+            this.minimize_button.TabIndex = 1;
+            this.minimize_button.UseVisualStyleBackColor = true;
+            this.minimize_button.Click += new System.EventHandler(this.minimize_button_Click);
+            // 
+            // fullscreen_button
+            // 
+            this.fullscreen_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fullscreen_button.Location = new System.Drawing.Point(1240, 0);
+            this.fullscreen_button.MaximumSize = new System.Drawing.Size(20, 20);
+            this.fullscreen_button.MinimumSize = new System.Drawing.Size(20, 20);
+            this.fullscreen_button.Name = "fullscreen_button";
+            this.fullscreen_button.Size = new System.Drawing.Size(20, 20);
+            this.fullscreen_button.TabIndex = 1;
+            this.fullscreen_button.UseVisualStyleBackColor = true;
+            this.fullscreen_button.Click += new System.EventHandler(this.maximize_button_Click);
+            // 
+            // tool_bar
+            // 
+            this.tool_bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(149)))), ((int)(((byte)(149)))));
+            this.tool_bar.Enabled = false;
+            this.tool_bar.Location = new System.Drawing.Point(0, 26);
+            this.tool_bar.Name = "tool_bar";
+            this.tool_bar.Size = new System.Drawing.Size(25, 687);
+            this.tool_bar.TabIndex = 3;
+            // 
+            // title_label
+            // 
+            this.title_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.title_label.AutoSize = true;
+            this.title_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.title_label.Location = new System.Drawing.Point(610, 4);
+            this.title_label.Name = "title_label";
+            this.title_label.Size = new System.Drawing.Size(59, 13);
+            this.title_label.TabIndex = 4;
+            this.title_label.Text = "Sketchpop";
+            // 
+            // left_settings_panel
+            // 
+            this.left_settings_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(149)))), ((int)(((byte)(149)))));
+            this.left_settings_panel.Enabled = false;
+            this.left_settings_panel.Location = new System.Drawing.Point(30, 26);
+            this.left_settings_panel.Name = "left_settings_panel";
+            this.left_settings_panel.Size = new System.Drawing.Size(172, 694);
+            this.left_settings_panel.TabIndex = 4;
+            // 
+            // quick_launch_bar
+            // 
+            this.quick_launch_bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(149)))), ((int)(((byte)(149)))));
+            this.quick_launch_bar.Location = new System.Drawing.Point(210, 26);
+            this.quick_launch_bar.Name = "quick_launch_bar";
+            this.quick_launch_bar.Size = new System.Drawing.Size(906, 36);
+            this.quick_launch_bar.TabIndex = 5;
+            // 
+            // canvas_panel
+            // 
+            this.canvas_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(144)))), ((int)(((byte)(144)))));
+            this.canvas_panel.Controls.Add(this.canvas_frame);
+            this.canvas_panel.Location = new System.Drawing.Point(210, 63);
+            this.canvas_panel.Name = "canvas_panel";
+            this.canvas_panel.Size = new System.Drawing.Size(906, 653);
+            this.canvas_panel.TabIndex = 6;
+            // 
+            // right_settings_panel
+            // 
+            this.right_settings_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(149)))), ((int)(((byte)(149)))));
+            this.right_settings_panel.Location = new System.Drawing.Point(1123, 26);
+            this.right_settings_panel.Name = "right_settings_panel";
+            this.right_settings_panel.Size = new System.Drawing.Size(154, 694);
+            this.right_settings_panel.TabIndex = 7;
+            // 
+            // canvas_frame
+            // 
+            this.canvas_frame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(167)))), ((int)(((byte)(167)))), ((int)(((byte)(167)))));
+            this.canvas_frame.Location = new System.Drawing.Point(0, 28);
+            this.canvas_frame.Name = "canvas_frame";
+            this.canvas_frame.Size = new System.Drawing.Size(906, 625);
+            this.canvas_frame.TabIndex = 0;
+            this.canvas_frame.TabStop = false;
+            // 
+            // main_window
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.right_settings_panel);
+            this.Controls.Add(this.canvas_panel);
+            this.Controls.Add(this.quick_launch_bar);
+            this.Controls.Add(this.left_settings_panel);
+            this.Controls.Add(this.tool_bar);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.fullscreen_button);
+            this.Controls.Add(this.minimize_button);
+            this.Controls.Add(this.title_bar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(128, 128);
+            this.Name = "main_window";
+            this.Text = "Sketchpop";
+            this.title_bar.ResumeLayout(false);
+            this.title_bar.PerformLayout();
+            this.canvas_panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas_frame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.Panel title_bar;
+        private System.Windows.Forms.Button fullscreen_button;
+        private System.Windows.Forms.Button minimize_button;
+        private System.Windows.Forms.Panel tool_bar;
+        private System.Windows.Forms.Label title_label;
+        private System.Windows.Forms.Panel left_settings_panel;
+        private System.Windows.Forms.Panel quick_launch_bar;
+        private System.Windows.Forms.Panel canvas_panel;
+        private System.Windows.Forms.Panel right_settings_panel;
+        private System.Windows.Forms.PictureBox canvas_frame;
+    }
+}
+
---
+++ Sketchpop/Main_Window_Form.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Sketchpop
+{
+    public partial class main_window : Form
+    {
+        public main_window()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void maximize_button_Click(object sender, EventArgs e)
+        {
+            this.WindowState = (this.WindowState == FormWindowState.Normal) ? FormWindowState.Maximized : FormWindowState.Normal;
+        }
+
+        private void minimize_button_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // From https://stackoverflow.com/q/2575216
+        private const int
+            HTCAPTION = 2,
+            HTLEFT = 10,
+            HTRIGHT = 11,
+            HTTOP = 12,
+            HTTOPLEFT = 13,
+            HTTOPRIGHT = 14,
+            HTBOTTOM = 15,
+            HTBOTTOMLEFT = 16,
+            HTBOTTOMRIGHT = 17;
+
+        private const int
+            _grip_size = 5, // window resize click border
+            _caption_size = 20; // window drag click border
+
+        private Rectangle top { get { return new Rectangle(0, 0, this.ClientSize.Width, _grip_size); } }
+        private Rectangle left { get { return new Rectangle(0, 0, _grip_size, this.ClientSize.Height); } }
+        private Rectangle bottom { get { return new Rectangle(0, this.ClientSize.Height - _grip_size, this.ClientSize.Width, _grip_size); } }
+        private Rectangle right { get { return new Rectangle(this.ClientSize.Width - _grip_size, 0, _grip_size, this.ClientSize.Height); } }
+
+        private Rectangle top_left { get { return new Rectangle(0, 0, _grip_size, _grip_size); } }
+        private Rectangle top_right { get { return new Rectangle(this.ClientSize.Width - _grip_size, 0, _grip_size, _grip_size); } }
+        private Rectangle bottom_left { get { return new Rectangle(0, this.ClientSize.Height - _grip_size, _grip_size, _grip_size); } }
+        private Rectangle bottom_right { get { return new Rectangle(this.ClientSize.Width - _grip_size, this.ClientSize.Height - _grip_size, _grip_size, _grip_size); } }
+
+        // To allow dragging and resizing the window without the default Window's form border
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == 0x84) // WM_NCHITTEST
+            {
+
+                // Dragging the window's position
+                Point pos = new Point(message.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < _caption_size)
+                {
+                    message.Result = (IntPtr)HTCAPTION;
+                }
+
+
+                // Resizing the window
+                var cursor = this.PointToClient(Cursor.Position);
+
+                if (top_left.Contains(cursor)) message.Result = (IntPtr)HTTOPLEFT;
+                else if (top_right.Contains(cursor)) message.Result = (IntPtr)HTTOPRIGHT;
+                else if (bottom_left.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMLEFT;
+                else if (bottom_right.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMRIGHT;
+
+                else if (top.Contains(cursor)) message.Result = (IntPtr)HTTOP;
+                else if (left.Contains(cursor)) message.Result = (IntPtr)HTLEFT;
+                else if (right.Contains(cursor)) message.Result = (IntPtr)HTRIGHT;
+                else if (bottom.Contains(cursor)) message.Result = (IntPtr)HTBOTTOM;
+            }
+        }
+    }
+}
