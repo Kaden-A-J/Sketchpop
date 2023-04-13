2cd24bc5f60734f6164562b049944f6edae740ce
Adrian_Flores <u0675923@umail.utah.edu>
2023-04-12 21:21:06 -0600

    created class for Unsplash Image for separation of concerns

---
+++ Sketchpop/Unsplash_Image.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketchpop
+{
+    public class UnsplashImage
+    {
+        // data from the Unsplash API call
+        private string id;
+        private string description;
+        private string author;
+        private string author_profile_link;
+        private string image_url; // image url from unsplash
+        private string unsplash_url;
+
+        public UnsplashImage(string id, string description, string author, string author_profile_link, string image_url, string unsplash_url)
+        {
+            this.id = id;
+            this.description = description;
+            this.author = author;
+            this.author_profile_link = author_profile_link;
+            this.image_url = image_url;
+            this.unsplash_url = unsplash_url;
+        }
+
+        public string Get_ID() { return id; }
+        public string Get_Description() { return description; }
+        public string Get_Author() { return author; }
+        public string Get_Author_Profile() { return author_profile_link; }
+        public string Get_Image_URL() { return image_url; }
+        public string Get_Unsplash_URL() { return unsplash_url; }
+    }
+}
