6abb84dc0e4b87a3b0b02c2e0472a657860d9984
Adrian_Flores <u0675923@umail.utah.edu>
2023-04-21 12:42:43 -0600

    added brush and brush manager classes for handling different brushes

--- Sketchpop/Brush_Manager.cs
+++ Sketchpop/Brush_Manager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -9,26 +10,40 @@ namespace Sketchpop
 {
     public class Brush_Manager
     {
-        internal class Brush
-        {
-            private string _name;
-            private int _stroke;
-            private Color _color;
-
-            public Brush(string name, int stroke_size, Color color)
-            {
-                _name = name;
-                _stroke = stroke_size;
-                _color = color;
-            }
-        }
-
-        List<Brush> _brushes;
+        private Dictionary<string, Brush> _brushes;
+        private Brush _current_brush;
 
         public Brush_Manager()
         {
-            _brushes = new List<Brush>();
+            _brushes = new Dictionary<string,Brush>();
+            _current_brush = new Brush("basic", 2, new SKColor(0,0,0,255));
+            
+            // add basic brush and eraser
+            _brushes.Add(_current_brush.Name(), _current_brush);
+            _brushes.Add("eraser", new Brush("eraser", 50, new SKColor(167, 167, 167, 255)));
         }
 
+        public void Set_Brush(string brush_name)
+        {
+            if(_brushes.ContainsKey(brush_name))
+            {
+                _current_brush = _brushes[brush_name];
+            }
+        }
+
+        public Brush Get_Current_Brush()
+        {
+            return _current_brush;
+        }
+
+        public void Add_Brush(Brush new_brush)
+        {
+            _brushes.Add(new_brush.Name(),new_brush);
+        }
+
+        public void Remove_Brush(Brush brush)
+        {
+            _brushes.Remove(brush.Name());
+        }
     }
 }
