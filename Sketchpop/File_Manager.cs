7207eb5021230cceb63fa73909ff7e8458b5b80a
kang-2020 <kangzhao2019@gmail.com>
2023-09-15 16:20:45 -0600

    some backend code added but there exist bugs.

--- Sketchpop/File_Manager.cs
+++ Sketchpop/File_Manager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +13,26 @@ namespace Sketchpop
 
         public void Save_as_EXC(string path, Layer_Manager layers) {
             Console.WriteLine(path);
-            for(int i = 0; i < layers.count; i++)
+            using (FileStream fs = File.Create(path))
+            using (BinaryWriter writer = new BinaryWriter(fs))
             {
-                // save each layer as a separate image
-                SkiaSharp.SKData encodedData = layers.get_image(i).EncodedData;
-                //System.IO.File.WriteAllBytes(path + i + ".png", encodedData.ToArray());
-               
+                // write header   
+                writer.Write("EXC");  
+                Console.WriteLine("layers count = "+layers.count);
+                writer.Write(layers.count);
+                Console.WriteLine("selected layer = "+layers.selected_layer);
+                writer.Write(layers.selected_layer);
+                for (int i = 1; i < layers.count; i++)
+                {
+                    Console.WriteLine("saving layer " + i);
+                    // save each layer as a separate image
+                    SKData encodedData = layers.get_image(i).EncodedData;
+                    writer.Write(encodedData.ToArray().Length);
+                    writer.Write(encodedData.ToArray());
+                    writer.Write(layers.get_layer_opacity(i));
+                }
             }
+     
         }
 
         public void Save_as_PNG(string path, Layer_Manager layers)
@@ -27,6 +42,27 @@ namespace Sketchpop
 
         public void Load_as_EXC(string path, Layer_Manager layers ) {
             // rebuild canvas
+            layers.reset();
+            using (FileStream fs = File.OpenRead(path))
+            using(BinaryReader reader = new BinaryReader(fs))
+            {
+                  // read header
+                String header = new String(reader.ReadChars(3));
+                if (header != "EXC")
+                {
+                    throw new Exception("Invalid file format");
+                }
+                int layer_count = reader.ReadInt32();
+                int selected_layer = reader.ReadInt32();
+                for (int i = 0; i < layer_count; i++)
+                {
+                    int img_size = reader.ReadInt32();
+                    byte[] img_data = reader.ReadBytes(img_size);
+                    float opacity = reader.ReadSingle();
+                    SKImage image = SKImage.FromEncodedData(img_data);
+                    layers.add_layer(image, opacity);
+                }
+            }
             
         }
 
