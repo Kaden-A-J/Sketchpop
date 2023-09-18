dd75d69a5ca00f64f267154e40bc3b37ceef8339
kang-2020 <kangzhao2019@gmail.com>
2023-09-18 16:50:24 -0600

    copy code

--- Sketchpop/File_Manager.cs
+++ Sketchpop/File_Manager.cs
@@ -5,21 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-
-namespace Sketchpop
-{
-    internal class File_Manager
-    {
-    }
-}
-using SkiaSharp;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Xml;
 
